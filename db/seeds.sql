3fade5c690ebd78ce74df1ae078debc172e5d3f4
Nicole Graiff <nicole.graiff@gmail.com>
2020-08-08 16:13:14 -0400

    Added db file containing schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (`burger_name`) VALUE ('Tofu Burger');
+INSERT INTO burgers (`burger_name`) VALUE ('Black Bean Burger');
+INSERT INTO burgers (`burger_name`) VALUE ('Portobello Burger');
