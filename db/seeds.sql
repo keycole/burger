db876cac5aaef45b61c25e659246caef8eb90953
Nicole Graiff <nicole.graiff@gmail.com>
2020-08-09 09:03:13 -0400

    Css and button updates to handlebars files. Added devoured items to seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,6 @@
 INSERT INTO burgers (`burger_name`) VALUE ('Tofu Burger');
 INSERT INTO burgers (`burger_name`) VALUE ('Black Bean Burger');
 INSERT INTO burgers (`burger_name`) VALUE ('Portobello Burger');
+INSERT INTO burgers (`burger_name`, `devoured`) VALUE ('Beyond Burger', 1);
+INSERT INTO burgers (`burger_name`, `devoured`) VALUE ('Impossible Burger', 1);
+INSERT INTO burgers (`burger_name`, `devoured`) VALUE ('Portobello Burger', 1);
